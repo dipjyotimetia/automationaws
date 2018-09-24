6e7c47ee0ce0179ed392b4d7771f2894e1ddfb54
Franca Moretto <fmoretto@l3949.seek.int>
2018-09-24 16:21:57 +1000

    minor changes

--- slackautomation/ec2cnfig.py
+++ slackautomation/ec2cnfig.py
@@ -27,3 +27,4 @@ inst.security_groups
 # authorize incoming connections from public ip via ssh
 sg = ec2.SecurityGroup(inst.security_groups[0]['GroupId'])
 sg.authorize_ingress(IpPermissions=[{'FromPort':22, 'ToPort':22, 'IpProtocol':'TCP', 'IpRanges':[{'CidrIp':'103.108.116.3/32'}]}])
+sg.authorize_ingress(IpPermissions=[{'FromPort':80, 'ToPort':80, 'IpProtocol':'TCP', 'IpRanges':[{'0.0.0.0/0'}]}])
