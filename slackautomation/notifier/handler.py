c050a3e343423eea95550a40b78d12ece6908de7
Franca Moretto <fmoretto@l3949.seek.int>
2018-09-26 13:24:09 +1000

    minor changes to config files due to different python versions

--- slackautomation/notifier/handler.py
+++ slackautomation/notifier/handler.py
@@ -1,8 +1,11 @@
 import os
+import requests
 
 def post_to_slack(event, context):
     print(event)
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
-
+    slack_message= "Testing" #From {} at {details[StartTime]}: detail{[Description]}".format(**event)
+    data = {"text": slack_message}
+    requests.post(slack_webhook_url, json=data)
     print(slack_webhook_url)
     return
